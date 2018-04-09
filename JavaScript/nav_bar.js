47c1ae6ad13280ce4ce5581131948ab6763d62d6
IsmailBBH <33698275+IsmailBBH@users.noreply.github.com>
2018-04-09 19:01:31 +0200

    1.0

---
+++ JavaScript/nav_bar.js
@@ -0,0 +1,10 @@
+
+function changerLogo ()
+{
+	document.getElementById("logo").src = "Images/LOGO.png";
+}
+
+function reprendreLogo ()
+{
+	document.getElementById("logo").src = "Images/logoBlanc.png";
+}
